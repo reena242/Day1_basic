43d879e7dce5e20525ea06e2b567c313b50464f4
reena242 <reenatechnicaltrainer@gmail.com>
2025-05-12 15:02:36 +0700

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //header
+int main()  // main function
+{    //body of main function
+    return 0;  
+    printf("happy coding");  //statements
+    return 0; // return statement
+}// end of main function
